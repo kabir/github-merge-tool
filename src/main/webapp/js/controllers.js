b385b4b63a4fd51dc8105b6addc76d1fd8a6e96c
Kabir Khan <kkhan@redhat.com>
2014-05-16 22:50:26 +0100

    wire up header controller

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -6,7 +6,11 @@ var mergeToolControllers = angular.module('mergeToolControllers', []);
 
 mergeToolControllers.controller('HeaderCtrl', ['$scope',
   function($scope) {
-    $scope.loggedin = true; //TODO Restify
+    //TODO Restify
+    $scope.header = {
+       loggedin:false,
+       admin:true
+    }
   }]);
 
 mergeToolControllers.controller('HomeCtrl', ['$scope', 
