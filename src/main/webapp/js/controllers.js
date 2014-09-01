b867354247e65bebe57f74c6a164bc1c8288f67e
Kabir Khan <kkhan@redhat.com>
2014-09-01 22:49:17 +0100

    More temp stuff breaking everything, index.html is now the main page

--- src/main/webapp/js/controllers.js
+++ src/main/webapp/js/controllers.js
@@ -2,22 +2,27 @@
 
 /* Controllers */
 
-var mergeToolControllers = angular.module('mergeToolControllers', []);
+//var mergeToolControllers = angular.module('mergeToolControllers', []);
+var mergeToolApp = angular.module('MergeToolApp', []);
 
-mergeToolControllers.controller('HeaderCtrl', ['$scope',
+//mergeToolControllers.controller('HeaderCtrl', ['$scope',
+mergeToolApp.controller('HeaderCtrl', ['$scope',
   function($scope) {
     //TODO Restify
     $scope.header = {
        loggedin:false,
-       admin:true
+       admin:false,
+       
     }
   }]);
 
-mergeToolControllers.controller('HomeCtrl', ['$scope', 
+//mergeToolControllers.controller('HomeCtrl', ['$scope',
+mergeToolApp.controller('HomeCtrl', ['$scope', 
   function($scope) {
   }]);
   
-mergeToolControllers.controller('LoginCtrl', ['$scope', 
+//mergeToolControllers.controller('LoginCtrl', ['$scope',
+mergeToolApp.controller('LoginCtrl', ['$scope', 
   function($scope) {
       $scope.failed = false;
       $scope.user = {};
